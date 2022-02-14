4cf5fafaff2315ff740078c1849a6eaeb5dd648b
sefamehmetyildiz <99538967+sefamehmetyildiz@users.noreply.github.com>
2022-02-14 22:59:40 +0300

    my practice files

---
+++ Ders 27.1 YaziRengiDegistirme.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("BU KURSUDA BURADA BITIRMIS OLDUK");
+    system("COLOR 5");
+
+    // 0 = BLACK
+    // 1 = BLUE
+    // 2 = GREEN
+    // 3 = AQUA
+    // 4 = RED
+    // 5 = PURPLE
+    // ...
+
+    getch();
+    return 0;
+}
